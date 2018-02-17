557612d5e48da8a816d692ab8bf34f6fbd0bcd84
TheFrankemon <francomontiel@hotmail.com>
2018-02-17 12:55:12 -0400

    Basic waypoints automatic movement for the player

--- PrototipoARPIL/Assets/Scripts/PlayerTruck.cs
+++ PrototipoARPIL/Assets/Scripts/PlayerTruck.cs
@@ -3,9 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerTruck : MonoBehaviour {
-	public int MovementSpeed = 3;
-	
+
+	public float MovementSpeed = 3;
+
+	public Transform[] wayPoints;
+	public int currentWayPoint = 0; 
+	Transform targetWayPoint;
+
 	void Update () {
+		//handleMovementByInput ();
+
+		if (currentWayPoint < this.wayPoints.Length) {
+			if (targetWayPoint == null)
+				targetWayPoint = wayPoints [currentWayPoint];
+			move ();
+		}
+	}
+
+	void handleMovementByInput() {
 		float hMovement = Input.GetAxis ("Horizontal");
 		float vMovement = Input.GetAxis ("Vertical");
 
@@ -13,4 +28,20 @@ public class PlayerTruck : MonoBehaviour {
 
 		transform.Translate (fixedSpeed * hMovement, 0, fixedSpeed * vMovement);
 	}
+
+	void move(){
+		float fixedSpeed = MovementSpeed * 0.1f;
+
+		transform.forward = Vector3.RotateTowards(transform.forward, targetWayPoint.position - transform.position, fixedSpeed, 0);
+		transform.position = Vector3.MoveTowards(transform.position, targetWayPoint.position, fixedSpeed);
+
+		if(transform.position == targetWayPoint.position) {
+			currentWayPoint++;
+
+			if (currentWayPoint == this.wayPoints.Length)
+				currentWayPoint = 0;
+
+			targetWayPoint = wayPoints[currentWayPoint];
+		}
+	} 
 }
