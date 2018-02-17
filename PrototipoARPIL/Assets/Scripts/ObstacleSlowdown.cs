fe361c693eefcc5836bf5b433fdfb30774112be2
TheFrankemon <francomontiel@hotmail.com>
2018-02-17 10:46:34 -0400

    Added folders

---
+++ PrototipoARPIL/Assets/Scripts/ObstacleSlowdown.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleSlowdown : MonoBehaviour {
+
+	void OnTriggerEnter (Collider col) {
+		//enemysgameobject.speed *=
+		//set timeout
+		Destroy (gameObject);
+	}
+}
