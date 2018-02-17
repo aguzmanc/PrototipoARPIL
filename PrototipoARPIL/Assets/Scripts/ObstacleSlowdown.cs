262534a454a278a3bd119dff369677b404535abb
TheFrankemon <francomontiel@hotmail.com>
2018-02-17 12:05:09 -0400

    BETA Slow down functionality working

--- PrototipoARPIL/Assets/Scripts/ObstacleSlowdown.cs
+++ PrototipoARPIL/Assets/Scripts/ObstacleSlowdown.cs
@@ -4,9 +4,25 @@ using UnityEngine;
 
 public class ObstacleSlowdown : MonoBehaviour {
 
+	[Range(1,5)]
+	public int SlowDownFactor = 2;
+
+	GameObject _player;
+	int _playerOriginalSpeed;
+
 	void OnTriggerEnter (Collider col) {
-		//enemysgameobject.speed *=
-		//set timeout
-		Destroy (gameObject);
+		if (SlowDownFactor == 0)
+			return;
+
+		_player = col.gameObject;
+		if (_player.CompareTag ("Player")) {
+			_playerOriginalSpeed = _player.GetComponent<PlayerTruck> ().MovementSpeed;
+
+			_player.GetComponent<PlayerTruck> ().MovementSpeed /= SlowDownFactor;
+		}
+	}
+
+	void OnTriggerExit(Collider col) {
+		_player.GetComponent<PlayerTruck> ().MovementSpeed = _playerOriginalSpeed;
 	}
 }
---
+++ PrototipoARPIL/Assets/Scripts/PlayerTruck.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTruck : MonoBehaviour {
+	public int MovementSpeed = 3;
+	
+	void Update () {
+		float hMovement = Input.GetAxis ("Horizontal");
+		float vMovement = Input.GetAxis ("Vertical");
+
+		float fixedSpeed = MovementSpeed * 0.1f;
+
+		transform.Translate (fixedSpeed * hMovement, 0, fixedSpeed * vMovement);
+	}
+}
