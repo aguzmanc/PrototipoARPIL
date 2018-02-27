363b139141d2608d411dfa2f1c374255cd45b96d
arn <spacerockganimedes@gmail.com>
2018-02-26 20:58:59 -0400

    Improved Road Generator

--- PrototipoARPIL/Assets/Scripts/PathCreator.cs
+++ PrototipoARPIL/Assets/Scripts/PathCreator.cs
@@ -34,45 +34,53 @@ public class PathCreator : MonoBehaviour
     {
         Vector3[] points = path.GetRawPoints(PointsPerSegment);
 
-        for(int i=0;i<points.Length;i++)
-            Debug.Log(points[i]);
-
         MeshFilter meshFilter = GetComponentInChildren<MeshFilter>();
 
         Vector3[] vs = new Vector3[(points.Length-1)*8];
         int[] ts = new int[(points.Length-1) * 6 * 3];
+
         Vector3 up = Vector3.up * Height;
 
+        int k;
+        Vector3 a=Vector3.zero, b=Vector3.zero, forward=Vector3.zero, right=Vector3.zero;
         for(int i=0;i<points.Length-1;i++) {
-            Vector3 a = points[i], b = points[i+1];
-            Vector3 forward = b-a;
-            Vector3 right = (Quaternion.AngleAxis(90, Vector3.up) * forward).normalized * Width;
+            a = points[i];
+            b = points[i+1];
 
-            int k=i*8;
+            forward = b-a;
+            right = (Quaternion.AngleAxis(90, Vector3.up) * forward).normalized * Width;
+
+            k=i*4;
 
             vs[k] = a+right;
-            vs[k+1] = a+right+forward;
-            vs[k+2] = a-right+forward;
+            vs[k+1] = a+right+up;
+            vs[k+2] = a-right+up;
             vs[k+3] = a-right;
-            vs[k+4] = a+right+up;
-            vs[k+5] = a+right+forward+up;
-            vs[k+6] = a-right+forward+up;
-            vs[k+7] = a-right+up;
 
             int[] tmp = new int[] {
-                k+6,k+5,k+4,
-                k+7,k+6,k+4,
-                k+5,k+1,k+0,
-                k+0,k+4,k+5,
-                k+7,k+3,k+2,
-                k+2,k+6,k+7
+                k,  k+1,k+4,
+                k+1,k+5,k+4,
+                k+1,k+2,k+5,
+                k+2,k+6,k+5 ,
+                k+3,k+7,k+6,
+                k+3,k+6,k+2
             };
-
-            //for(int k=0;k<18;k++) 
-              //  ts[i*18+k] = tmp[k];
 
             System.Array.Copy(tmp, 0, ts, i*18, tmp.Length);
         }
+
+        // Last segment
+        /*
+        a = points[points.Length-1];
+        b = points[points.Length-2];
+        forward = a-b;
+        right = (Quaternion.AngleAxis(90, Vector3.up) * forward).normalized * Width;
+        k = (points.Length-1) * 4;
+        vs[k] =   a+right;
+        vs[k+1] = a+right+up;
+        vs[k+2] = a-right+up;
+        vs[k+3] = a-right;
+        */
 
         Mesh mesh = new Mesh();
         meshFilter.mesh = mesh;
