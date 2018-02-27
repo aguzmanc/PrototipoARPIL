8cbd0b9cb638236cf1751f613f02722460dbe02c
arn <spacerockganimedes@gmail.com>
2018-02-26 20:09:37 -0400

    First working Path Generator

--- PrototipoARPIL/Assets/Scripts/PathCreator.cs
+++ PrototipoARPIL/Assets/Scripts/PathCreator.cs
@@ -10,6 +10,8 @@ public class PathCreator : MonoBehaviour
     [Range(2, 100)]
     public int PointsPerSegment = 30;
     public GameObject TestPointPrototype;
+    public float Width = 0.2f;
+    public float Height = 0.1f;
 
 
 	public void CreatePath()
@@ -26,11 +28,9 @@ public class PathCreator : MonoBehaviour
 
     public void GenerateTestPoints()
     {
-
-
     }
 
-    public void GenerateMesh(float width=1f, float height=0.1f)
+    public void GenerateMesh()
     {
         Vector3[] points = path.GetRawPoints(PointsPerSegment);
 
@@ -41,35 +41,37 @@ public class PathCreator : MonoBehaviour
 
         Vector3[] vs = new Vector3[(points.Length-1)*8];
         int[] ts = new int[(points.Length-1) * 6 * 3];
-        Vector3 up = Vector3.up * height;
+        Vector3 up = Vector3.up * Height;
 
-        for(int i=2;i<3;i++) {
+        for(int i=0;i<points.Length-1;i++) {
             Vector3 a = points[i], b = points[i+1];
             Vector3 forward = b-a;
-            Vector3 right = (Quaternion.AngleAxis(90, Vector3.up) * forward).normalized * width;
+            Vector3 right = (Quaternion.AngleAxis(90, Vector3.up) * forward).normalized * Width;
 
-            vs[i] = a+right;
-            vs[i+1] = a+right+forward;
-            vs[i+2] = a-right+forward;
-            vs[i+3] = a-right;
-            vs[i+4] = a+right+up;
-            vs[i+5] = a+right+forward+up;
-            vs[i+6] = a-right+forward+up;
-            vs[i+7] = a-right+up;
+            int k=i*8;
+
+            vs[k] = a+right;
+            vs[k+1] = a+right+forward;
+            vs[k+2] = a-right+forward;
+            vs[k+3] = a-right;
+            vs[k+4] = a+right+up;
+            vs[k+5] = a+right+forward+up;
+            vs[k+6] = a-right+forward+up;
+            vs[k+7] = a-right+up;
 
             int[] tmp = new int[] {
-                i+6,i+5,i+4,
-                i+7,i+6,i+4,
-                i+5,i+1,i+0,
-                i+0,i+4,i+5,
-                i+7,i+3,i+2,
-                i+2,i+6,i+7
+                k+6,k+5,k+4,
+                k+7,k+6,k+4,
+                k+5,k+1,k+0,
+                k+0,k+4,k+5,
+                k+7,k+3,k+2,
+                k+2,k+6,k+7
             };
 
-            for(int k=0;k<18;k++) 
-                ts[i*18+k] = tmp[k];
+            //for(int k=0;k<18;k++) 
+              //  ts[i*18+k] = tmp[k];
 
-            //System.Array.Copy(tmp, 0, ts, i*18, tmp.Length);
+            System.Array.Copy(tmp, 0, ts, i*18, tmp.Length);
         }
 
         Mesh mesh = new Mesh();
