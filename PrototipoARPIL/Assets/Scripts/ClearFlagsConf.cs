00aa885b0d3319e765fbf454ede030b1c416ada6
Allan Leon <aleon.107@outlook.com>
2018-02-28 19:46:07 -0400

    Testing shadows on small objects

---
+++ PrototipoARPIL/Assets/Scripts/ClearFlagsConf.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClearFlagsConf : MonoBehaviour {
+
+	Camera arCamera;
+
+	// Use this for initialization
+	void Start () {
+		arCamera = GetComponent<Camera> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		arCamera.clearFlags = CameraClearFlags.Depth;
+	}
+}
---
+++ PrototipoARPIL/Assets/Scripts/RotatorFromMarker.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatorFromMarker : MonoBehaviour {
+
+	public Quaternion _defaultRotation;
+	public Vector3 _defaultForward;
+	public Transform ARMarkerMovable;
+	public Transform ARMarkerFixed;
+	public float Speed = 0.1f;
+	public float Accuracy = 5f;
+	public bool IsActive;
+
+	// Use this for initialization
+	void Start () {
+		_defaultRotation = transform.rotation;
+		_defaultForward = transform.forward;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		IsActive = ARMarkerMovable.gameObject.activeInHierarchy;
+		if (ARMarkerMovable.gameObject.activeInHierarchy) {
+			//float distance = Vector3.Distance (ARMarkerMovable.position, ARMarkerFixed.position);
+			Vector3 difference = ARMarkerMovable.position - ARMarkerFixed.position;
+			float distance = difference.magnitude;
+			int direction = difference.x  > 0 ? 1 : -1;
+			Vector3 newRotation = Quaternion.Euler (distance * Accuracy * distance, 0, 0) * _defaultForward;
+			transform.rotation = Quaternion.Slerp (transform.rotation, Quaternion.Euler(newRotation), Time.time * Speed);
+		} else {
+			transform.rotation = Quaternion.Slerp (transform.rotation, _defaultRotation, Time.time * Speed);
+		}
+	}
+}
