f342ad21bf5870d6ad2f0c3795d26bdc1ad2761e
arn <spacerockganimedes@gmail.com>
2018-03-09 08:24:01 -0400

    Correccion del camino

--- PrototipoARPIL/Assets/Scripts/PathCreator.cs
+++ PrototipoARPIL/Assets/Scripts/PathCreator.cs
@@ -33,92 +33,111 @@ public class PathCreator : MonoBehaviour
  
     public Vector3[] ALLPOINTS;
 
+
+	public List<int> faces = new List<int>();
+
     public void GenerateMesh()
     {
         MeshFilter meshFilter = GetComponentInChildren<MeshFilter>();
 
         Vector3[] points = path.GetRawPoints(PointsPerSegment);
-        Debug.Log(points.Length);
         ALLPOINTS = points;
 
         Vector3 up = Vector3.up * Height;
         List<Vector3> vs = new List<Vector3>();
-        List<Vector3> normals = new List<Vector3>();
 
+		List<Vector3> normals = new List<Vector3> ();
+		normals.AddRange (new Vector3[points.Length * 12]);
+
+		faces = new List<int> ();
         Vector3 a=Vector3.zero, b=Vector3.zero, forward=Vector3.zero, right=Vector3.zero;
 
+		// generate cuts first
+		List<Vector3[]> cuts = new List<Vector3[]> ();
+
+		for (int i = 0; i < points.Length; i++) {
+			if(i<points.Length-2) { // for all the points
+				a = points[i];
+				b = points[i+1];
+			} else { // for the last point
+				a = points[i-1];
+				b = path.isClosed ? points [0] : points [i];
+			}
+
+			forward = b-a;
+			right = (Quaternion.AngleAxis(90, Vector3.up) * forward).normalized * Width;
+
+			cuts.Add (
+				new Vector3[4] {
+					a + right,	
+					a + right + up,
+					a - right + up,
+					a - right
+				});
+		}
+
+
         // generate vertices
+		Vector3[] VA = null, VB=null;
         for(int i=0;i<points.Length;i++) {
             if(i<points.Length-2) { // for all the points
-                a = points[i];
-                b = points[i+1];
-
-                forward = b-a;
-                right = (Quaternion.AngleAxis(90, Vector3.up) * forward).normalized * Width;
-
-                vs.Add(a+right);
-                vs.Add(a+right+up);
-                vs.Add(a-right+up);
-                vs.Add(a-right);
+				VA = cuts[i];
+                VB = cuts[i+1];
             } else { // for the last point
-                a = points[i-1];
-                b = points[i];
-
-                forward = b-a;
-                right = (Quaternion.AngleAxis(90, Vector3.up) * forward).normalized * Width;
-
-                vs.Add(b+right);
-                vs.Add(b+right+up);
-                vs.Add(b-right+up);
-                vs.Add(b-right);
+				VA = cuts[i-1];
+				VB = path.isClosed ? cuts[0] : cuts[i];
             }
-        }
 
-        List<int> faces = new List<int>();
+			vs.Add (VA [0]);
+			vs.Add (VA [1]);
+			vs.Add (VB [1]);
+			vs.Add (VB [0]);
 
-        for(int i=0;i<points.Length-1;i++) {
-            a = points[i];
-            b = points[i+1];
+			vs.Add (VA [1]);
+			vs.Add (VA [2]);
+			vs.Add (VB [2]);
+			vs.Add (VB [1]);
 
-            forward = b-a;
-            right = (Quaternion.AngleAxis(90, Vector3.up) * forward).normalized * Width;
+			vs.Add (VA [2]);
+			vs.Add (VA [3]);
+			vs.Add (VB [3]);
+			vs.Add (VB [2]);
 
-            int[] tmp = new int[] {
-                0,1,4,
-                1,5,4,
-                1,2,5,
-                2,6,5,
-                3,7,6,
-                3,6,2
-            };
+			int[] tmp = new int[] {
+				0,1,2,
+				0,2,3, 
+				4,5,6,
+				4,6,7,
+				8,9,10,
+				8,10,11
+			};
 
-            for(int k=0;k<tmp.Length;k++){
-                int vertexNumber = i*4 + tmp[k];
-                faces.Add(vertexNumber);
+			Vector3[] face = new Vector3[3];
+			int[] idxVert = new int[3];
 
-                if(k%3==2){
-                    
-                    Vector3 va = vs[tmp[k-2]];
-                    Vector3 vb = vs[tmp[k-1]];
-                    Vector3 vc = vs[tmp[k]];
+			for(int k=0;k<tmp.Length;k++){
+				int vertexNumber = i*12 + tmp[k];
+				faces.Add(vertexNumber);
 
-                    normals.Add(Vector3.Cross(vb - va, vc - va));
-                    normals.Add(Vector3.Cross(vb - va, vc - va));
-                    normals.Add(Vector3.Cross(vb - va, vc - va));
-                }
-            }
+				face [k % 3] = vs [vertexNumber];
+				idxVert [k % 3] = vertexNumber;
+
+				if(k%3==2){
+					Vector3 normal = Vector3.Cross (face [1] - face [0],face [2] - face [0]);
+
+					normals [idxVert [0]] = normal;
+					normals [idxVert [1]] = normal;
+					normals [idxVert [2]] = normal;
+				}
+			}
         }
 
         Mesh mesh = new Mesh();
         meshFilter.mesh = mesh;
         mesh.Clear();
 
-        Debug.Log(vs.Count + "-----" + normals.Count);
-
-        //mesh.
-
         mesh.vertices = vs.ToArray();
         mesh.triangles = faces.ToArray();
-        //mesh.normals = normals.ToArray();
+        mesh.normals = normals.ToArray();
     }
 }
