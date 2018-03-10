b538a92dbb170f0c513bbebef1320b0f0063f54f
TheFrankemon <francomontiel@hotmail.com>
2018-03-10 12:08:16 -0400

    Created gull model and partially made generation script

---
+++ PrototipoARPIL/Assets/Scripts/GullGenerator.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GullGenerator : MonoBehaviour {
+
+	public Transform Gully;
+
+	Vector3 NW;
+	Vector3 SE;
+	Vector3 NE;
+	Vector3 SW;
+
+	void Start () {
+		NW = new Vector3(-40, 30, 40);
+		SE = new Vector3(40, 30, -40);
+		NE = new Vector3(40, 30, 40);
+		SW = new Vector3(-40, 30, -40);
+	}
+	
+	void GenerateGulls() {
+		int gullQuantity = Random.Range(3, 9);
+		for(int i = 0; i <= gullQuantity; i++) {
+			Instantiate(Gully, NW, new Quaternion(0, 0, 0, 0));
+		}
+	}
+
+	void Update () {
+		
+	}
+}
